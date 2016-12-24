c037827df4a14f04401f28aca70f786f049e323c
Ron Mizrahi <ronmizr@gmail.com>
2016-12-25 00:14:49 +0200

    Version 2.0
    Added Database

--- matala2maps/app/src/main/java/com/example/ron/matala2maps/MainActivity.java
+++ matala2maps/app/src/main/java/com/example/ron/matala2maps/MainActivity.java
@@ -1,31 +1,76 @@
 package com.example.ron.matala2maps;
 import android.app.Activity;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import android.provider.Settings.Secure;
 
 public class MainActivity extends AppCompatActivity{
     Button btn_gps,btn_qr;
     final Activity activity=this;
+    DatabaseReference mRootRef;
+    DatabaseReference mUserRef;
+    public User currentUser = null;
+    public String userKey=null;
+    String currentAndroidID=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //database references
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        mUserRef = mRootRef.child("User");
         btn_gps=(Button) findViewById(R.id.btn_gps);
         btn_qr =(Button) findViewById(R.id.btn_qr);
+        currentAndroidID=Secure.getString(getContentResolver(), Secure.ANDROID_ID);
         init();
     }
 
     private void init()
     {
+        mUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //loop twice to make sure u get the object key
+                for(int i=0;i<2;i++)
+                {
+                    for (DataSnapshot user : dataSnapshot.getChildren()) {
+                        User u = user.getValue(User.class);
+                        if (u.getAndroidID().equals(currentAndroidID)) {
+                            currentUser = u;
+                            userKey = user.getKey();
+                            break;  //user was found
+                        }
+                    }
+                    if (currentUser == null) {
+                        currentUser = new User(currentAndroidID, false);
+                        mUserRef.push().setValue(currentUser);
+                        //loop to find the key after adding
+                    }
+                    else
+                        break;  //key is already exist get out
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         btn_gps.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -71,4 +116,5 @@ public class MainActivity extends AppCompatActivity{
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
 }
---
+++ matala2maps/app/src/main/java/com/example/ron/matala2maps/User.java
@@ -0,0 +1,44 @@
+package com.example.ron.matala2maps;
+
+/**
+ * Created by Vitaligever on 24/12/2016.
+ */
+
+public class User {
+    private boolean isAdmin;
+    private String androidID;
+    private UserLocation location=null;
+
+    public User()
+    {}
+
+    public User(String androidID, boolean isAdmin) {
+        this.androidID = androidID;
+        this.isAdmin = isAdmin;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getAndroidID() {
+        return androidID;
+    }
+
+    public void setAndroidID(String androidID) {
+        this.androidID = androidID;
+    }
+
+    public UserLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(UserLocation location) {
+        this.location = location;
+    }
+}
+
